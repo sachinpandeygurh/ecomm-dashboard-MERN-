062a2d3720d264044428b95f6956ec745d17bbec
sachinpandeygurh <sachingurh@gmail.com>
2023-06-25 01:39:31 +0530

    frontendSetup

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import NavBar from './nav';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter className="App-header">
+       <NavBar/>
+      <Routes>
+        <Route path="/" element={<h1>Products</h1>} />
+        <Route path="/add" element={<h1>Add Products</h1>} />
+        <Route path="/update" element={<h1>Update Products</h1>} />
+        <Route path="/logout" element={<h1>Logout</h1>} />
+        <Route path="/profile" element={<h1>Profile</h1>} />
+        
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
