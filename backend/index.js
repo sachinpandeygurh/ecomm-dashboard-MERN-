07d191890b82b1d5ff4d0e61dc2212c05de0a8a9
sachinpandeygurh <sachingurh@gmail.com>
2023-06-25 21:37:40 +0530

    commit#10

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors')
 require('./db/config')
 const User = require('./db/User')
 const app = express();
 
 app.use(express.json())
+app.use(cors())
 
 app.post('/register',async(req, resp)=>{
     let user = new User(req.body)
