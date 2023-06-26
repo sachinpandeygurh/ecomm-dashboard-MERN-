f89e46137bc120eb0adbd6fc23acae81c5367ce9
sachinpandeygurh <sachingurh@gmail.com>
2023-06-27 00:23:06 +0530

    callAddProductAPI#20

--- frontend/src/components/addProduct.jsx
+++ frontend/src/components/addProduct.jsx
@@ -1,69 +1,82 @@
 import React, { useState } from "react";
 
-const AddProduct =()=>{
-    const [name, setName] = useState("");
-    const [price, setPrice] = useState("");
-    const [category, setCategory] = useState("");
-    const [company, setCompany] = useState("");
-    return (
-        <div className="container mt-5">
-          <div className="row justify-content-center">
-            <div className="col-lg-6">
-              <div className="card">
-                <div className="card-body">
-                  <h1 className="text-center">Add Product</h1>
-                  <div className="form-group">
-                    <input
-                      type="text"
-                      className="form-control inputBox my-3"
-                      placeholder="Product Name"
-                      value={name}
-                      onChange={(e) => setName(e.target.value)}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <input
-                      type="text"
-                      className="form-control inputBox my-3"
-                      placeholder="Product Price"
-                      value={price}
-                      onChange={(e) => setPrice(e.target.value)}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <input
-                      type="text"
-                      className="form-control inputBox my-3"
-                      placeholder="Product Category"
-                      value={category}
-                      onChange={(e) => setCategory(e.target.value)}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <input
-                      type="text"
-                      className="form-control inputBox my-3"
-                      placeholder="Product Company Name"
-                      value={company}
-                      onChange={(e) => setCompany(e.target.value)}
-                    />
-                  </div>
-                  <div className="form-group">
-                    <button
-                      className="form-control btn btn-primary appButton my-3"
-                      onClick={""}
-                      type="button"
-                    >
-                      Submit
-                    </button>
-                  </div>
-                </div>
+const AddProduct = () => {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [category, setCategory] = useState("");
+  const [company, setCompany] = useState("");
+
+  const addProduct = async () => {
+    const userId = JSON.parse(localStorage.getItem("user"))._id;
+    let result = await fetch("http://localhost:5000/addProduct", {
+      method: "POST",
+      body: JSON.stringify({ name, price, category, company, userId }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.warn(result);
+  };
+
+  return (
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-lg-6">
+          <div className="card">
+            <div className="card-body">
+              <h1 className="text-center">Add Product</h1>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control inputBox my-3"
+                  placeholder="Product Name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control inputBox my-3"
+                  placeholder="Product Price"
+                  value={price}
+                  onChange={(e) => setPrice(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control inputBox my-3"
+                  placeholder="Product Category"
+                  value={category}
+                  onChange={(e) => setCategory(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control inputBox my-3"
+                  placeholder="Product Company Name"
+                  value={company}
+                  onChange={(e) => setCompany(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <button
+                  className="form-control btn btn-primary appButton my-3"
+                  onClick={addProduct}
+                  type="button"
+                >
+                  Submit
+                </button>
               </div>
             </div>
           </div>
         </div>
-      );
-      
-}
+      </div>
+    </div>
+  );
+};
 
 export default AddProduct;
