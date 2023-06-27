73cd4fbe83f1b63bd72528844a6c2a6edd83d9a4
sachinpandeygurh <sachingurh@gmail.com>
2023-06-27 13:57:02 +0530

    Prefill update form#28

--- frontend/src/components/updateProduct.js
+++ frontend/src/components/updateProduct.js
@@ -1,10 +1,28 @@
 import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const UpdateProduct = () => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [category, setCategory] = useState("");
   const [company, setCompany] = useState("");
+  const params = useParams();
+
+  useEffect(() => {
+    getProductDetails();
+  }, []);
+
+
+  const getProductDetails = async ()=>{
+    console.warn(params);
+    let result = await fetch(`http://localhost:5000/product/${params.id}`)
+    result = await result.json()
+    console.warn(result);
+  setName (result.name)
+  setPrice (result.price)
+  setCategory (result.category)
+  setCompany (result.company)
+  }
 
   const updateProduct = async () => {
     console.warn(name, price, category, company);
@@ -25,7 +43,6 @@ const UpdateProduct = () => {
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                 />
-            
               </div>
               <div className="form-group">
                 <input
@@ -35,7 +52,6 @@ const UpdateProduct = () => {
                   value={price}
                   onChange={(e) => setPrice(e.target.value)}
                 />
-               
               </div>
               <div className="form-group">
                 <input
@@ -45,7 +61,6 @@ const UpdateProduct = () => {
                   value={category}
                   onChange={(e) => setCategory(e.target.value)}
                 />
-               
               </div>
               <div className="form-group">
                 <input
@@ -55,7 +70,6 @@ const UpdateProduct = () => {
                   value={company}
                   onChange={(e) => setCompany(e.target.value)}
                 />
-              
               </div>
               <div className="form-group">
                 <button
