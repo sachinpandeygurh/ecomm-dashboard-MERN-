ab2077ac99eb37bde20f2b7ba368b8e268207adc
sachinpandeygurh <sachingurh@gmail.com>
2023-06-27 14:58:42 +0530

    Update Product API#29

--- backend/index.js
+++ backend/index.js
@@ -66,6 +66,15 @@ app.get('/product/:id', async (req, resp) => {
   }
 });
 
+app.put('/product/:id', async (req, resp) => {
+  const result = await Product.updateOne(
+    { _id: req.params.id },
+    {
+      $set: req.body
+    }
+    );
+  resp.send(result);
+});
 
 
 app.listen(5000) 
